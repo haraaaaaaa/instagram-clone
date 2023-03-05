33e63c1955c036bcac498e91de6c11d644c5df5d
haraaaaaaa <harunhebibovic05@gmail.com>
2023-03-05 02:49:56 +0100

    res&req handling

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
+const postsData = path.join(__dirname, 'views', 'posts.json')
+
+
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 
@@ -9,11 +12,24 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (request, response) => {
-  response.render("index", { pageTitle: "Home" });
+  fs.readFile(postsData, (err, posts) => {
+    response.render("index", { pageTitle: "Home", posts: JSON.parse() });
+  })
 });
 
 app.get("/post", (request, response) => {
   response.render("index", { pageTitle: "Post" });
 });
+
+app.post("/post", (request, response) => {
+  const posts = req.body
+  fs.readFile(postsData, (err, posts) => {
+    const updatedPosts = [posts, ...JSON.parse(posts)]
+    fs.writeFile(postsData, JSON.stringify(updatedPosts), () => {
+      response.redirect("/")
+    })
+  })
+
+})
 
 app.listen(5000);
