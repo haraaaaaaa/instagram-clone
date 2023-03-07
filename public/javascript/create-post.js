5a0dc2c0a9586aea099c18d102a443dd6e01ef01
Puskass <cosovictarik711@gmail.com>
2023-03-07 18:16:26 +0100

    Modal works but not quite

--- public/javascript/create-post.js
+++ public/javascript/create-post.js
@@ -7,3 +7,24 @@ const gotoHomePage = async (event) => {
 };
 
 homePage.addEventListener("click", gotoHomePage);
+
+// Modal 
+
+// Selectors
+const btnShare = document.getElementById('share');
+const modal = document.getElementById('modal');
+const yesBtn = document.getElementById('yes-btn');
+const noBtn = document.getElementById('no-btn');
+
+// Event Listeners
+btnShare.addEventListener('click', () => {
+  modal.classList.remove('hidden');
+});
+
+yesBtn.addEventListener('click', () => {
+  modal.classList.add('hidden');
+});
+
+noBtn.addEventListener('click', () => {
+  modal.classList.add('hidden');
+});
